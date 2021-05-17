8597f4a5dbaa9f1c54a5398fa1d9036e02ad2428
HeshanHH <heshanwijerathna01@gmail.com>
2021-05-17 20:11:01 +0530

    First testing route

--- ecommers-back-end/src/server.js
+++ ecommers-back-end/src/server.js
@@ -2,6 +2,13 @@ const express = require('express');
 require('dotenv').config(); // import .env
 const app = express();
 
+// http://localhost:PORT/
+app.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Hello from server',
+  });
+});
+
 const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => {
   console.log(`Server app is running on port ${PORT}`);
