7341cc723e81e7d63a51f3e4cf274786391e4d64
HeshanHH <heshanwijerathna01@gmail.com>
2021-05-22 20:54:22 +0530

    Replace Inputs with Generic Input UI Component

--- ecommers-admin-panel/src/components/UI/Input/Input.js
+++ ecommers-admin-panel/src/components/UI/Input/Input.js
@@ -5,7 +5,12 @@ const Input = (props) => {
   return (
     <Form.Group controlId="formBasicEmail">
       <Form.Label>{props.label}</Form.Label>
-      <Form.Control type={props.type} placeholder={props.placeholder} />
+      <Form.Control
+        type={props.type}
+        placeholder={props.placeholder}
+        value={props.value}
+        onChange={props.onChange}
+      />
       <Form.Text className="text-muted">{props.errorMessage}</Form.Text>
     </Form.Group>
   );
--- ecommers-admin-panel/src/containers/Signup/Signup.js
+++ ecommers-admin-panel/src/containers/Signup/Signup.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Form, Row, Col, Button } from 'react-bootstrap';
 import Layout from '../../components/Layout/Layout';
+import Input from '../../components/UI/Input/Input';
 
 const Signup = (props) => {
   return (
@@ -12,29 +13,47 @@ const Signup = (props) => {
               <Form>
                 <Row>
                   <Col md={6}>
-                    <Form.Group controlId="formBasicPassword">
+                    <Input
+                      label="First Name"
+                      type="text"
+                      placeholder="First Name"
+                      value=""
+                      onChange={() => {}}
+                    />
+
+                    {/* <Form.Group controlId="formBasicPassword">
                       <Form.Label>First Name</Form.Label>
                       <Form.Control type="text" placeholder="First Name" />
-                    </Form.Group>
+                    </Form.Group> */}
                   </Col>
                   <Col md={6}>
-                    <Form.Group controlId="formBasicPassword">
-                      <Form.Label>Last Name</Form.Label>
-                      <Form.Control type="text" placeholder="Last Name" />
-                    </Form.Group>
+                    <Input
+                      label="Last Name"
+                      type="text"
+                      placeholder="Last Name"
+                      value=""
+                      onChange={() => {}}
+                    />
                   </Col>
                 </Row>
                 <Form.Group controlId="formBasicEmail">
-                  <Form.Label>Email address</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email" />
-                  <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                  </Form.Text>
+                  <Input
+                    label="Email"
+                    type="email"
+                    placeholder="Email"
+                    value=""
+                    onChange={() => {}}
+                  />
                 </Form.Group>
 
                 <Form.Group controlId="formBasicPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" />
+                  <Input
+                    label="Password"
+                    type="password"
+                    placeholder="Password"
+                    value=""
+                    onChange={() => {}}
+                  />
                 </Form.Group>
                 {/* <Form.Group controlId="formBasicCheckbox">
                   <Form.Check type="checkbox" label="Check me out" />
